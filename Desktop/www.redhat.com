47f94f3a6dbeda6a38bd117ba55a769efbc25c6b
kunaltyagi <tyagi.kunal@live.com>
2014-03-02 17:34:58 +0530

    major changes

--- Desktop/www.redhat.com
+++
@@ -1,4 +0,0 @@
-[Desktop Entry]
-Icon=/usr/share/pixmaps/redhat/shadowman-round-48.png
-Type=Link
-URL=http://www.redhat.com/
---
+++ devel/cult.php
@@ -0,0 +1,56 @@
+﻿<?php
+	include	'header.php';
+	menu('activities');
+?>
+
+            <!-- MAIN -->
+			<div id="main" class="cf">
+				
+				<div id="project-gallery">
+					<div class="thumb" ><img src="img/dummies/slides/home_cult_pic01.jpg" alt="Thumbnail" /></div>
+					<div class="thumb" ><img src="img/dummies/slides/cult_pic02.jpg" alt="Thumbnail" /></div>
+					<div class="thumb" ><img src="img/dummies/slides/cult_pic03.jpg" alt="Thumbnail" /></div>
+					<div class="thumb" ><img src="img/dummies/slides/cult_pic04.jpg" alt="Thumbnail" /></div>
+				</div>
+				
+				<div id="project-info">
+					<div id="project-title">Cultural Activities</div>
+					
+					<p>Do you imagine yourself going ba-dum-tss on the drums or enacting a drunken Gabbar? <br/>Do you see yourself nailing questions on anything under the Sun in a quiz or doing a moonwalk which would make Michael Jackson turn in his grave? <br/>Do you see yourself playing the hero in a (romantic?) short film or painting a political caricature which makes the thrones in Delhi tremble? <br/><br/>If your answer is yes to any of the above, welcome aboard – you are on just the right flight! While you are on board, you shall witness some of the most delectable performances of dance, music and acting in the institute by your own hostelmates and get a chance to be a part of the same. <br/><br/>You shall learn how to solve some of the most mind-boggling crosswords, capture beautiful sceneries in a DSLR and edit a video to make Manmohan Singh sing or Rahul Dravid do a Gangnam Style, perhaps. Meanwhile, you also get to fight it out for your hostel in the General Championships and revel in the glory of victory!</p>
+					<br/>
+					    <blockquote >With the above and a lot more in store <br/>You’ll have one heck of a joyride for sure!</blockquote>
+       					<h3>VI – are the – KINGS!</h3>					
+					<div id="project-sheet">
+						<p><strong>Current GC Standings</strong><br/>5</p>
+						<p><a href="http://www.luiszuno.com" target="_blank" class="action-button">View Tally</a></p>
+					</div>
+				</div>
+				
+				<!-- related -->
+				<div class="cf"></div>
+				<div class="related-projects">
+					<div class="related-heading">Snapshots</div>
+					<ul class="related-list cf">
+						<li>
+							<a href="#" class="thumb"><img src="img/dummies/slides/cult_snapshot01.jpg" alt="" /></a>
+							<a href="#" class="heading">Is smthg for diff generes of cult required?</a>
+						</li>
+						<li>
+							<a href="#" class="thumb"><img src="img/dummies/f-02.jpg" alt="" /></a>
+							<a href="#" class="heading">Link for some Cult Portals</a>
+						</li>
+						<li>
+							<a href="https://www.facebook.com/groups/271045126241711/" class="thumb"><img src="img/dummies/f-03.jpg" alt="" /></a>
+							<a href="https://www.facebook.com/groups/271045126241711/" class="heading">Facebook Page</a>
+						</li>
+					</ul>
+				</div>
+				<!-- ENDS related -->			
+			
+					
+			</div>
+			<!-- ENDS MAIN -->
+
+<?php
+	include 'footer.php'
+?>
